89fa3087f3e695e515996d9db937c497737cf746
Rafael Jegundo <rafael.jegundo@gmail.com>
2019-08-15 18:06:15 +0100

    Change deployment params according to thisDAO values

--- deployment-params.js
+++ deployment-params.js
@@ -2,9 +2,9 @@
 module.exports.SUMMONER = ''
 module.exports.TOKEN = ''
 module.exports.PERIOD_DURATION_IN_SECONDS = 17280
-module.exports.VOTING_DURATON_IN_PERIODS = 35
-module.exports.GRACE_DURATON_IN_PERIODS = 35
+module.exports.VOTING_DURATON_IN_PERIODS = 15
+module.exports.GRACE_DURATON_IN_PERIODS = 15
 module.exports.ABORT_WINDOW_IN_PERIODS = 5
-module.exports.PROPOSAL_DEPOSIT = '1000000000000000000' // Large numbers should be string or big numbers
+module.exports.PROPOSAL_DEPOSIT = '10000000000000000' // Large numbers should be string or big numbers
 module.exports.DILUTION_BOUND = 3
-module.exports.PROCESSING_REWARD = '10000000000'
+module.exports.PROCESSING_REWARD = '100000000'
